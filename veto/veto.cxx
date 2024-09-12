e8fb36d815e565e78fe9ac41ef003602f19ab6ca
lfpc <luisfelipe1998@gmail.com>
2024-09-12 18:11:31 +0200

    Add lines of sensitive plane - test

--- veto/veto.cxx
+++ veto/veto.cxx
@@ -1066,10 +1066,10 @@ void veto::ConstructGeometry()
     decayVolumeMed = gGeoManager->GetMedium(decayVolumeMed_name);   // decay volume, air/helium/vacuum
 
     TGeoVolume* tDecayVol = new TGeoVolumeAssembly("DecayVolume");
-    //TGeoVolume *Tracker = gGeoManager->MakeBox("Tracker",Sens,1000.-0.01,900.-0.01,1.);
-    //  Tracker->SetLineColor(kMagenta-10);
-    //  top->AddNode(Tracker, 1, new TGeoTranslation(0, +100, fT1z));
-    //  AddSensitiveVolume(Tracker);
+    TGeoVolume *Tracker = gGeoManager->MakeBox("Tracker",Sens,1000.-0.01,900.-0.01,1.);
+    Tracker->SetLineColor(kMagenta-10);
+    top->AddNode(Tracker, 1, new TGeoTranslation(0, +100, fT1z));
+    //AddSensitiveVolume(Tracker);
 
     TGeoVolume* seg = MakeSegments();
     tDecayVol->AddNode(seg, 1, new TGeoTranslation(0, 0, 0));
