43457a05cb321abd84fec59fae41afb027bfb83b
lfpc <luisfelipe1998@gmail.com>
2024-09-27 16:52:25 +0200

    small cavern

--- passive/ShipMuonShield.cxx
+++ passive/ShipMuonShield.cxx
@@ -839,8 +839,8 @@ void ShipMuonShield::ConstructGeometry()
 		 gapOut, Z);
 
       // Create TCC8 tunnel around muon shield
-      Double_t TCC8_length =  30 * m;
-      Double_t ECN3_length =  55 * m;
+      Double_t TCC8_length =  25 * m;
+      Double_t ECN3_length =  10 * m;
       Double_t TCC8_trench_length = 12 * m;
       Double_t zgap = 10 * cm;
       Double_t absorber_offset = zgap;
